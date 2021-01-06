b3876ac804b8cb7ab02f487aca61593de546b318
Janny K <JannyKosin@gmail.com>
2021-01-05 21:31:36 -0800

    add build

---
+++ shopping-cart/build/precache-manifest.8d5211a5121b0c9ec84f97371e4bfdcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba9860357c3556d882e6932fc80b458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26083c06dfb006b816eb",
+    "url": "/static/css/main.d0005989.chunk.css"
+  },
+  {
+    "revision": "12ad4a0ecf4b32354102",
+    "url": "/static/js/2.e31b9c72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e31b9c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26083c06dfb006b816eb",
+    "url": "/static/js/main.efc11fd6.chunk.js"
+  },
+  {
+    "revision": "81d08e04a2e4215dd2ef",
+    "url": "/static/js/runtime-main.5856509f.js"
+  }
+]);
